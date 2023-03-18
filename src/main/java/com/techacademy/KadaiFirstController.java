8fe96c1e9bf05572357a00100315d4eec9d423b8
OYAWAMA <fluting.dalton_0m@icloud.com>
2023-03-18 22:33:42 +0900

    kadai14done

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -2,7 +2,6 @@ package com.techacademy;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +17,32 @@ public class KadaiFirstController {
     }
     @GetMapping("dayofweek/{val1}")
     public String dispDayOfWeek(@PathVariable String val1) {
-        LocalDate day = LocalDate.parse(val1,DateTimeFormatter.ofPattern("yyyyMMdd") );
-        DayOfWeek x =day.getDayOfWeek();
-        return "指定日：" +x.toString();
+        LocalDate DAY = LocalDate.parse(val1,DateTimeFormatter.ofPattern("yyyyMMdd") );
+        DayOfWeek DAYOFWEEK =DAY.getDayOfWeek();
+        return "指定日：" + DAYOFWEEK.toString().substring(0,1)+DAYOFWEEK.toString().substring(1).toLowerCase();
+    }
+    @GetMapping("plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 + val2;
+        return "計算結果：" + res;
+    }
+    @GetMapping("minus/{val1}/{val2}")
+    public String calcMinus(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 - val2;
+        return "計算結果：" + res;
+    }
+    @GetMapping("times/{val1}/{val2}")
+    public String calcTimes(@PathVariable int val1, @PathVariable int val2) {
+        int res = 0;
+        res = val1 * val2;
+        return "計算結果：" + res;
+    }
+    @GetMapping("divide/{val1}/{val2}")
+    public String calcDivide(@PathVariable float val1, @PathVariable int val2) {
+        float res = 0;
+        res = val1 / val2;
+        return "計算結果：" + res;
     }
 }
